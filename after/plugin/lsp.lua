90fbd9e3bde663d80f2bc2f26b8afe4c05e21ab5
Shlomo Y <shlomo@techtio.io>
2025-07-25 15:48:24 +0300

    add css to lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,9 +1,32 @@
----default config for luals
+-- lsp.lua
+-- Setup TypeScript Language Server for JS/TS files
+vim.lsp.config('ts_ls', {
+  cmd = {'typescript-language-server', '--stdio'},
+  filetypes = {'javascript', 'javascriptreact', 'typescript', 'typescriptreact'},
+  root_markers = {'tsconfig.json', 'jsconfig.json', 'package.json', '.git'},
+})
+
+-- Setup Lua Language Server for Lua files only
 vim.lsp.config('luals', {
   cmd = {'lua-language-server'},
-  filetypes = {'lua', 'js', 'ts', 'jsx', 'tsx', 'json', 'jsonc'},
+  filetypes = {'lua'},
   root_markers = {'.luarc.json', '.luarc.jsonc'},
 })
 
+-- Setup CSS Language Server for CSS/SCSS/LESS files
+vim.lsp.config('cssls', {
+  cmd = {'vscode-css-language-server', '--stdio'},
+  filetypes = {'css', 'scss', 'less'},
+  root_markers = {'package.json', '.git'},
+})
+
+-- Enable all servers
+vim.lsp.enable('ts_ls')
 vim.lsp.enable('luals')
-vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition)
+vim.lsp.enable('cssls')
+
+-- Set up keymaps
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
