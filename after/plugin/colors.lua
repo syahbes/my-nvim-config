5a0fb78dfda294ba08b7baf928cb58e238e5b694
Shlomo Y <shlomo@techtio.io>
2025-07-31 22:06:13 +0300

    bgcolor

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,9 @@
 vim.cmd.colorscheme("catppuccin")
--- Override line number colors
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#7f849c" })
+
+-- Override background color
+vim.api.nvim_set_hl(0, "Normal", { bg = "#171422" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "#171422" })  -- non-active window
+
+-- Override line number color
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#6a6e84" })
+
