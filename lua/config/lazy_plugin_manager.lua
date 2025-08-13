91125238d3a7ab166609ab99ae7cabfe821bb0b6
Shlomo Y <shlomo@techtio.io>
2025-08-13 10:23:30 +0300

    done

--- lua/config/lazy_plugin_manager.lua
+++ lua/config/lazy_plugin_manager.lua
@@ -34,5 +34,5 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "catppuccin-mocha" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  --  checker = { enabled = true },
 })
