971a98f28b249d5e4c1c5a2e8bd9a92e6f967cb0
Shlomo Y <shlomo@techtio.io>
2025-08-25 13:30:21 +0300

    theme

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,5 +1,5 @@
 -- vim.opt.guicursor = "" --cursor as block
-vim.opt.cursorline = true -- highlight current line
+--vim.opt.cursorline = true -- highlight current line
 vim.opt.nu = true -- Show line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
 
