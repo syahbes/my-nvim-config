8e5424fd2c75c60d9a395aeab2e58a3aceaf5b51
Shlomi Y. <syahbes@gmail.com>
2025-12-12 22:03:16 +0200

    popup

--- lua/config/plugins/marko.lua
+++ lua/config/plugins/marko.lua
@@ -1,11 +1,17 @@
 return {
-	"developedbyed/marko.nvim",
+	"syahbes/marko.nvim",
 	config = function()
 		require("marko").setup({
 			width = 100,
 			height = 100,
 			border = "rounded",
 			title = " Marks ",
+			virtual_text = {
+				enabled = true,
+				icon = "*",
+				hl_group = "Comment",
+				position = "eol",  -- Changed from "eol" to "overlay"
+			},
 		})
 	end,
 }
